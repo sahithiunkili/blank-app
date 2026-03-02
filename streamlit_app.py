616d3e1ee90c6f58261345858163fdf6d0a6cc17
Sahithi Unkili <58786353+sahithiunkili@users.noreply.github.com>
2026-03-01 20:55:01 -0500

    Update Streamlit app title and content
    trying if streamlit works

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import pandas as pd
+ 
+st.write("""
+# My first app
+Hello *world!*
+""")
